f095f1aa84f0001f64355e0657ce859749ec1e40
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2021-09-23 20:14:42 -0500

    Implimented
    Class - AppUser - with set varables
    Interface - IAppUserRepository - with Methods

---
+++ CSC470-P3/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC470_P3
+{
+    class AppUser
+    {
+        public string UserName;
+        public string Password;
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+    }
+}
---
+++ CSC470-P3/IAppUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC470_P3
+{
+    interface IAppUserRepository
+    {
+        bool Login(string UserName, string Password);
+        List<AppUser> GetAll();
+        void SetAuthentication(string UserName, bool isAuthenticated);
+        AppUser GetByUserName(string UserName);
+    }
+}
