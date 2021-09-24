a48f47196002e272a400204fb2b37549f4859322
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2021-09-23 21:28:59 -0500

    Error, Need to pull last Commit

--- CSC470-P3/AppUser.cs
+++ CSC470-P3/AppUser.cs
@@ -12,8 +12,11 @@ namespace CSC470_P3
         public string LastName;
         public string EmailAddress;
 <<<<<<< HEAD
+<<<<<<< HEAD
         public bool IsAuthenticated; //missed during set up of this class
                                     // Travis : Thanks, I missed this in the set up
+=======
+>>>>>>> parent of 538ae2c (FakeAppUserRepository set up)
 =======
 >>>>>>> parent of 538ae2c (FakeAppUserRepository set up)
     }
