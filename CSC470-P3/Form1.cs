e6357ac8144bd3fe1a00f1252a32f8b0b333b4b6
JHeidelB <67404904+JHeidelB@users.noreply.github.com>
2021-10-05 22:40:59 -0500

    Added Menu Bar with dropdowns

--- CSC470-P3/Form1.cs
+++ CSC470-P3/Form1.cs
@@ -20,6 +20,7 @@ namespace CSC470_P3
         private void LoginButton_Click(object sender, EventArgs e)
         {            
             // Travis: The Program wasnot working because you left the commented out code in when you were testing
+            // Jamae: Thanks, didnt mean to leave that in there, I was trying to figrue out how to load forms, with some sort of authetication
             //if (UserNameText.Text == "travis" && PasswordText.Text == "0000")
             {
                 bool isAuthenticated;
@@ -31,6 +32,7 @@ namespace CSC470_P3
                     check.SetAuthentication(UserNameText.Text, isAuthenticated);
                     Main mainPage = new Main();
                     this.Hide(); // Travis: I changed this to Hide, Close exits the program
+                                // Jamae: I had this as Hide but in the Sequence Diagram it is listed as "Close()"
                     mainPage.Show();
                 }
                 else
--- CSC470-P3/Main.Designer.cs
+++ CSC470-P3/Main.Designer.cs
@@ -29,12 +29,237 @@ namespace CSC470_P3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MainMenuStip = new System.Windows.Forms.MenuStrip();
+            this.PreferencesStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.SelectProjectStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CreateProjectStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ModifyProjectStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.RemoveProjectStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.PreferencesStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.IssueStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.DashboardStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.RecordStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.RemoveStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.RequirementStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.DesignStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TestStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ReportStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CountByAssigneeStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ListByAssigneeStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CountByArtifactStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ListByArtifactStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CountByDateRangeStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ListByDateRangeStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TrendByDateRangeStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ReportStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.MainMenuStip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainMenuStip
+            // 
+            this.MainMenuStip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.MainMenuStip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PreferencesStripMenuItem1,
+            this.IssueStripMenuItem2,
+            this.RequirementStripMenuItem1,
+            this.DesignStripMenuItem1,
+            this.TestStripMenuItem1});
+            this.MainMenuStip.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuStip.Name = "MainMenuStip";
+            this.MainMenuStip.Size = new System.Drawing.Size(1223, 40);
+            this.MainMenuStip.TabIndex = 0;
+            this.MainMenuStip.Text = "mainMenu";
+            // 
+            // PreferencesStripMenuItem1
+            // 
+            this.PreferencesStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SelectProjectStripMenuItem1,
+            this.CreateProjectStripMenuItem1,
+            this.ModifyProjectStripMenuItem1,
+            this.RemoveProjectStripMenuItem1,
+            this.PreferencesStripTextBox1});
+            this.PreferencesStripMenuItem1.Name = "PreferencesStripMenuItem1";
+            this.PreferencesStripMenuItem1.Size = new System.Drawing.Size(158, 36);
+            this.PreferencesStripMenuItem1.Text = "Preferences";
+            // 
+            // SelectProjectStripMenuItem1
+            // 
+            this.SelectProjectStripMenuItem1.Name = "SelectProjectStripMenuItem1";
+            this.SelectProjectStripMenuItem1.Size = new System.Drawing.Size(313, 44);
+            this.SelectProjectStripMenuItem1.Text = "Select Project";
+            // 
+            // CreateProjectStripMenuItem1
+            // 
+            this.CreateProjectStripMenuItem1.Name = "CreateProjectStripMenuItem1";
+            this.CreateProjectStripMenuItem1.Size = new System.Drawing.Size(313, 44);
+            this.CreateProjectStripMenuItem1.Text = "Create Project";
+            // 
+            // ModifyProjectStripMenuItem1
+            // 
+            this.ModifyProjectStripMenuItem1.Name = "ModifyProjectStripMenuItem1";
+            this.ModifyProjectStripMenuItem1.Size = new System.Drawing.Size(313, 44);
+            this.ModifyProjectStripMenuItem1.Text = "Modify Project";
+            // 
+            // RemoveProjectStripMenuItem1
+            // 
+            this.RemoveProjectStripMenuItem1.Name = "RemoveProjectStripMenuItem1";
+            this.RemoveProjectStripMenuItem1.Size = new System.Drawing.Size(313, 44);
+            this.RemoveProjectStripMenuItem1.Text = "Remove Project";
+            // 
+            // PreferencesStripTextBox1
+            // 
+            this.PreferencesStripTextBox1.Name = "PreferencesStripTextBox1";
+            this.PreferencesStripTextBox1.Size = new System.Drawing.Size(100, 39);
+            // 
+            // IssueStripMenuItem2
+            // 
+            this.IssueStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DashboardStripMenuItem1,
+            this.RecordStripMenuItem1,
+            this.RemoveStripMenuItem1,
+            this.ReportStripMenuItem1});
+            this.IssueStripMenuItem2.Name = "IssueStripMenuItem2";
+            this.IssueStripMenuItem2.Size = new System.Drawing.Size(87, 36);
+            this.IssueStripMenuItem2.Text = "Issue";
+            // 
+            // DashboardStripMenuItem1
+            // 
+            this.DashboardStripMenuItem1.Name = "DashboardStripMenuItem1";
+            this.DashboardStripMenuItem1.Size = new System.Drawing.Size(262, 44);
+            this.DashboardStripMenuItem1.Text = "Dashboard";
+            // 
+            // RecordStripMenuItem1
+            // 
+            this.RecordStripMenuItem1.Name = "RecordStripMenuItem1";
+            this.RecordStripMenuItem1.Size = new System.Drawing.Size(262, 44);
+            this.RecordStripMenuItem1.Text = "Record";
+            // 
+            // RemoveStripMenuItem1
+            // 
+            this.RemoveStripMenuItem1.Name = "RemoveStripMenuItem1";
+            this.RemoveStripMenuItem1.Size = new System.Drawing.Size(262, 44);
+            this.RemoveStripMenuItem1.Text = "Remove";
+            // 
+            // RequirementStripMenuItem1
+            // 
+            this.RequirementStripMenuItem1.Name = "RequirementStripMenuItem1";
+            this.RequirementStripMenuItem1.Size = new System.Drawing.Size(171, 36);
+            this.RequirementStripMenuItem1.Text = "Requirement";
+            // 
+            // DesignStripMenuItem1
+            // 
+            this.DesignStripMenuItem1.Name = "DesignStripMenuItem1";
+            this.DesignStripMenuItem1.Size = new System.Drawing.Size(108, 36);
+            this.DesignStripMenuItem1.Text = "Design";
+            // 
+            // TestStripMenuItem1
+            // 
+            this.TestStripMenuItem1.Name = "TestStripMenuItem1";
+            this.TestStripMenuItem1.Size = new System.Drawing.Size(76, 36);
+            this.TestStripMenuItem1.Text = "Test";
+            // 
+            // ReportStripMenuItem1
+            // 
+            this.ReportStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CountByAssigneeStripMenuItem1,
+            this.ListByAssigneeStripMenuItem1,
+            this.CountByArtifactStripMenuItem1,
+            this.ListByArtifactStripMenuItem1,
+            this.CountByDateRangeStripMenuItem1,
+            this.ListByDateRangeStripMenuItem1,
+            this.TrendByDateRangeStripMenuItem1,
+            this.ReportStripTextBox1});
+            this.ReportStripMenuItem1.Name = "ReportStripMenuItem1";
+            this.ReportStripMenuItem1.Size = new System.Drawing.Size(262, 44);
+            this.ReportStripMenuItem1.Text = "Report";
+            // 
+            // CountByAssigneeStripMenuItem1
+            // 
+            this.CountByAssigneeStripMenuItem1.Name = "CountByAssigneeStripMenuItem1";
+            this.CountByAssigneeStripMenuItem1.Size = new System.Drawing.Size(376, 44);
+            this.CountByAssigneeStripMenuItem1.Text = "Count by Assignee";
+            // 
+            // ListByAssigneeStripMenuItem1
+            // 
+            this.ListByAssigneeStripMenuItem1.Name = "ListByAssigneeStripMenuItem1";
+            this.ListByAssigneeStripMenuItem1.Size = new System.Drawing.Size(376, 44);
+            this.ListByAssigneeStripMenuItem1.Text = "List by Assignee";
+            // 
+            // CountByArtifactStripMenuItem1
+            // 
+            this.CountByArtifactStripMenuItem1.Name = "CountByArtifactStripMenuItem1";
+            this.CountByArtifactStripMenuItem1.Size = new System.Drawing.Size(376, 44);
+            this.CountByArtifactStripMenuItem1.Text = "Counbt by Artifact";
+            // 
+            // ListByArtifactStripMenuItem1
+            // 
+            this.ListByArtifactStripMenuItem1.Name = "ListByArtifactStripMenuItem1";
+            this.ListByArtifactStripMenuItem1.Size = new System.Drawing.Size(376, 44);
+            this.ListByArtifactStripMenuItem1.Text = "List by Artifact";
+            // 
+            // CountByDateRangeStripMenuItem1
+            // 
+            this.CountByDateRangeStripMenuItem1.Name = "CountByDateRangeStripMenuItem1";
+            this.CountByDateRangeStripMenuItem1.Size = new System.Drawing.Size(376, 44);
+            this.CountByDateRangeStripMenuItem1.Text = "Count by Date Range";
+            // 
+            // ListByDateRangeStripMenuItem1
+            // 
+            this.ListByDateRangeStripMenuItem1.Name = "ListByDateRangeStripMenuItem1";
+            this.ListByDateRangeStripMenuItem1.Size = new System.Drawing.Size(376, 44);
+            this.ListByDateRangeStripMenuItem1.Text = "List by Date Range";
+            // 
+            // TrendByDateRangeStripMenuItem1
+            // 
+            this.TrendByDateRangeStripMenuItem1.Name = "TrendByDateRangeStripMenuItem1";
+            this.TrendByDateRangeStripMenuItem1.Size = new System.Drawing.Size(376, 44);
+            this.TrendByDateRangeStripMenuItem1.Text = "Trend by Date Range";
+            // 
+            // ReportStripTextBox1
+            // 
+            this.ReportStripTextBox1.Name = "ReportStripTextBox1";
+            this.ReportStripTextBox1.Size = new System.Drawing.Size(100, 39);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1223, 893);
+            this.Controls.Add(this.MainMenuStip);
+            this.MainMenuStrip = this.MainMenuStip;
+            this.Name = "Main";
             this.Text = "Main";
+            this.MainMenuStip.ResumeLayout(false);
+            this.MainMenuStip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenuStip;
+        private System.Windows.Forms.ToolStripMenuItem PreferencesStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem SelectProjectStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem IssueStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem RequirementStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem DesignStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem TestStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CreateProjectStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ModifyProjectStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem RemoveProjectStripMenuItem1;
+        private System.Windows.Forms.ToolStripTextBox PreferencesStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem DashboardStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem RecordStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem RemoveStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ReportStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CountByAssigneeStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ListByAssigneeStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CountByArtifactStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ListByArtifactStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CountByDateRangeStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ListByDateRangeStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem TrendByDateRangeStripMenuItem1;
+        private System.Windows.Forms.ToolStripTextBox ReportStripTextBox1;
     }
 }
