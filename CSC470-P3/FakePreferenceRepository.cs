a4cd89ab6746ddeeeed50ebb5cc075a7df8dca6a
JHeidelB <67404904+JHeidelB@users.noreply.github.com>
2021-10-14 22:55:07 -0500

    Added Bottom section of UML

---
+++ CSC470-P3/FakePreferenceRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC470_P3
+{
+    class FakePreferenceRepository : IPreperenceRepository
+    {
+        public const string PREFERENCE_PROJECT_ID = "Project ID";
+        public const string PREFERENCE_PROJECT_NAME = "Project Name";
+        private const string NO_ERROR = "";
+        private static Dictionary<string, Dictionary<string, string>> Preferences = new Dictionary<string, Dictionary<string, string>>();
+
+        public string GetPreference(string UserName, string PreferenceName)
+        {
+            Dictionary<string, string> NameValuePair = new Dictionary<string, string>();
+            string value = "";
+            if (Preferences.TryGetValue(UserName, out NameValuePair))
+            {
+                NameValuePair.TryGetValue(PreferenceName, out value);
+            }
+            return value;
+        }
+
+        public string SetPreference(string UserName, string PreferenceName, string Value)
+        {
+            Dictionary<string, Dictionary<string, string>> preference = new Dictionary<string, Dictionary<string, string>>();
+            string value = "";
+            return value;
+        }
+    }
+}
---
+++ CSC470-P3/Preference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC470_P3
+{
+    class Preference
+    {
+        string UserName;
+        string Name;
+        string Value;
+    }
+}
