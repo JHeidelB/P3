aec09047cc8edc15930091fdb57b57e2966520ef
JHeidelB <67404904+JHeidelB@users.noreply.github.com>
2021-11-16 23:13:46 -0600

    Worked with the Modify section
    I got stuck at a problem ForEach, it would not allow me to iderate through the Dictionart

---
+++ P5/IIssueRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    public interface IIssueRepository
+    {
+        string Add(int ID, Issue issue);
+       
+    }
+}
