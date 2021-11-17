899b4d91ded5209a286b6919093d10c22bba387f
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2021-11-16 22:21:03 -0600

    Created Issue.cs
    Created Issue to hold the values of the issues Dictionary

---
+++ P5/Issue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class Issue
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string discoveryDate { get; set; }
+        public string discoverer { get; set; }
+        public string inicialDiscription { get; set; }
+        public string component { get; set; }
+        public string status { get; set; }
+    }
+}
