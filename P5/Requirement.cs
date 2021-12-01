58eb2c92e5060482c1daa0762e57b28ea008c19a
JHeidelB <67404904+JHeidelB@users.noreply.github.com>
2021-11-30 22:18:58 -0600

    Added Class Requirements, Select Feature

---
+++ P5/Requirement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Requirement
+    {
+        public int ID { get; set; }
+        public int ProjectId { get; set; }
+        public int Feature { get; set; }
+        public string Statement { get; set; }
+
+    }
+}
