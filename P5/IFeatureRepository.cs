daf952246ad3ac3b5236bf019a95434d3c38c1ff
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2021-11-30 21:40:19 -0600

    interface for Featrue Repository Added

---
+++ P5/IFeatureRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    interface IFeatureRepository
+    {
+        string Add(Feature featrure);
+        List<Feature> GetAll(int ProjectID);
+        string Remove(Feature feature);
+        string Modify(Feature feature);
+        Feature GetFeatureById(int projectID, int featureId);
+        Feature GetFeatureByTirle(int projectID, string title);
+    }
+}
