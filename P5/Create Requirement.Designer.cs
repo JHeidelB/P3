fcb5e34d7b3281168128ab947256eec89ed610d4
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2021-11-30 21:10:36 -0600

    Create Requirement Form Added

---
+++ P5/Create Requirement.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Builder
+{
+    partial class Create_Requirement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFeature = new System.Windows.Forms.Label();
+            this.textBoxFeature = new System.Windows.Forms.TextBox();
+            this.labelStatement = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonCreateRequirement = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFeature
+            // 
+            this.labelFeature.AutoSize = true;
+            this.labelFeature.Location = new System.Drawing.Point(115, 60);
+            this.labelFeature.Name = "labelFeature";
+            this.labelFeature.Size = new System.Drawing.Size(79, 25);
+            this.labelFeature.TabIndex = 0;
+            this.labelFeature.Text = "Feature";
+            this.labelFeature.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxFeature
+            // 
+            this.textBoxFeature.Location = new System.Drawing.Point(201, 60);
+            this.textBoxFeature.Name = "textBoxFeature";
+            this.textBoxFeature.Size = new System.Drawing.Size(587, 29);
+            this.textBoxFeature.TabIndex = 1;
+            this.textBoxFeature.TextChanged += new System.EventHandler(this.textBoxFeature_TextChanged);
+            // 
+            // labelStatement
+            // 
+            this.labelStatement.AutoSize = true;
+            this.labelStatement.Location = new System.Drawing.Point(93, 117);
+            this.labelStatement.Name = "labelStatement";
+            this.labelStatement.Size = new System.Drawing.Size(101, 25);
+            this.labelStatement.TabIndex = 2;
+            this.labelStatement.Text = "Statement";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(201, 117);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(587, 414);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(416, 564);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(128, 44);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonCreateRequirement
+            // 
+            this.buttonCreateRequirement.Enabled = false;
+            this.buttonCreateRequirement.Location = new System.Drawing.Point(565, 564);
+            this.buttonCreateRequirement.Name = "buttonCreateRequirement";
+            this.buttonCreateRequirement.Size = new System.Drawing.Size(223, 44);
+            this.buttonCreateRequirement.TabIndex = 5;
+            this.buttonCreateRequirement.Text = "Create Requirement";
+            this.buttonCreateRequirement.UseVisualStyleBackColor = true;
+            this.buttonCreateRequirement.Click += new System.EventHandler(this.buttonCreateRequirement_Click);
+            // 
+            // Create_Requirement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 666);
+            this.Controls.Add(this.buttonCreateRequirement);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelStatement);
+            this.Controls.Add(this.textBoxFeature);
+            this.Controls.Add(this.labelFeature);
+            this.Name = "Create_Requirement";
+            this.Text = "Create_Requirement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFeature;
+        private System.Windows.Forms.TextBox textBoxFeature;
+        private System.Windows.Forms.Label labelStatement;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonCreateRequirement;
+    }
+}
---
+++ P5/Create Requirement.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Builder
+{
+    public partial class Create_Requirement : Form
+    {
+        private string featureTitle;
+
+        public Create_Requirement(string featureName)
+        {
+            this.featureTitle = featureName;
+            this.CenterToParent();
+            textBoxFeature.Text = featureName;
+        }
+
+        
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonCreateRequirement_Click(object sender, EventArgs e)
+        {
+            Requirement CreateReq = new Requirement();
+            FakeRequirementRepository Req = new FakeRequirementRepository();
+            CreateReq.Feature = textBoxFeature.Text;
+            CreateReq.Statement = textBox1.Text;
+            Req.Add(CreateReq);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            setButtonVis();
+        }
+        private void setButtonVis()
+        {
+            if (textBox1.Text != String.Empty)
+            {
+                buttonCreateRequirement.Enabled = true;
+            }
+            else
+            {
+                buttonCreateRequirement.Enabled = false;
+            }
+        }
+
+        private void textBoxFeature_TextChanged(object sender, EventArgs e)
+        {
+            setTextVis();
+        }
+
+        private void setTextVis()
+        {
+            if (textBoxFeature.Text != String.Empty)
+            {
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                textBox1.Enabled = false;
+            }
+        }
+    }
+}
